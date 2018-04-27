fb1574a62806087026ccf602a504a8c0b2a2d613
Michelle Olivieri <michelleo72@gmail.com>
2018-04-27 01:23:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def intialize(name, grade, id = nil)
+  def initialize(name, grade, id = nil)
     @id = id
     @name = name
     @grade = grade
